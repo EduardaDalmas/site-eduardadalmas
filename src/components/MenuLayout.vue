45d6a2d0dd120e6cd69c61a9ff5ecf799a447f55
DalmasEduarda <eduarda.dalmas@doctorclin.com.br>
2022-12-30 14:24:28 -0300

    removiso trechos inutilizados

--- src/components/MenuLayout.vue
+++ src/components/MenuLayout.vue
@@ -37,9 +37,6 @@
       </div>
     </q-scroll-area>
   </q-drawer>
-  $(window).scroll(function() { var scroll = $(window).scrollTop(); if (scroll >
-  500) { $('.menu').css('background-color','yellow'); } else {
-  $('.menu').css('background-color','blue'); } });
 </template>
 
 <script>
